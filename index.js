005e82b432c4664298ad34b23427a469f0f9b256
nevicoder <nevi@Nevi-T450s.localdomain>
2023-02-03 18:54:06 +0700

    final commit

--- index.js
+++ index.js
@@ -9,24 +9,28 @@ const VND = new Intl.NumberFormat("vi-VN", {
   currency: "VND",
 });
 Bot.onText(/\/crypto (.+)/, (msg, match) => {
-  const crypto = match[1];
-  console.log(crypto);
-  axios
-    .get(
-      `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${process.env.CMC_KEY}&symbol=${crypto}&convert=VND`
-    )
-    .then((response) => {
-      const data = response.data.data;
-      for (const coin in data) {
-        const [result] = data[coin];
-        const price = result.quote.VND.price;
-        console.log(price);
-        Bot.sendMessage(
-          msg.chat.id,
-          `Tên: ${result.name}
-Giá: ${VND.format(price)}`
-        );
-      }
-    })
-    .catch((err) => Bot.sendMessage(msg.chat.id, "Tên coin không đúng"));
+  try {
+    const crypto = match[1];
+    console.log(crypto);
+    axios
+      .get(
+        `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${process.env.CMC_KEY}&symbol=${crypto}&convert=VND`
+      )
+      .then((response) => {
+        const data = response.data.data;
+        for (const coin in data) {
+          const [result] = data[coin];
+          const price = result.quote.VND.price;
+          console.log(price);
+          Bot.sendMessage(
+            msg.chat.id,
+            `Tên: ${result.name}
+  Giá: ${VND.format(price)}`
+          );
+        }
+      })
+      .catch((err) => Bot.sendMessage(msg.chat.id, "Tên coin không đúng"));
+  } catch (e) {
+    console.log(e);
+  }
 });
